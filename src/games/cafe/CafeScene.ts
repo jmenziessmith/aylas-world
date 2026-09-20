8273d90099ea99792e70ecc32f1ac78a79785c1c
Jon Menzies-Smith <github@menzies-smith.co.uk>
2026-09-20 13:22:27 +0100

    Lower cafe walking destination friend

--- src/games/cafe/CafeScene.ts
+++ src/games/cafe/CafeScene.ts
@@ -401,7 +401,7 @@ export class CafeScene extends Phaser.Scene {
     const journey = CAFE_LAYOUT.walker;
     const journeyProgress = this.steps / this.round.order.steps;
     this.carryBackground?.setX(this.carryBackgroundX(journeyProgress));
-    this.art(this.customerKey(), 865, 130, 90, 104, 'Friend');
+    this.art(this.customerKey(), 865, 170, 90, 104, 'Friend');
     this.art('table', 860, 220, 170, 104, 'Table');
     this.walker = this.art('ayla-carry', journey.startX + journeyProgress * (journey.endX - journey.startX), journey.y, 180, 170, 'Ayla');
     const bar = CAFE_LAYOUT.progress;
